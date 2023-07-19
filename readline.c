c656534c253a22948f8b8bb930af32a6c8451938
Rophpad <rophenp@gmail.com>
2023-07-19 07:42:54 -0700

    readline function implemented

---
+++ readline.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * readline - Function that will take user input
+ *
+ * Return: The text entered by the user
+ */
+char *readline(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+			exit(EXIT_SUCCESS);
+		else
+		{
+			perror("Error: readline");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return (line);
+}
