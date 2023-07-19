c866c7ebc81b764d5ae61040bf904f3d32a7eb6b
Rophpad <rophenp@gmail.com>
2023-07-19 08:40:34 -0700

    parse function implemented

---
+++ parse.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * parse - parse the line entered by user
+ * transform it in a string without some delimiter
+ * @line: the line entered by the user
+ *
+ * Return: A set of string terminated by NULL
+ */
+char **parse(char *line)
+{
+	/*64 is choosed arbitrary*/
+	int bufsize = 64;
+	int position = 0;
+	char **tokens = malloc(sizeof(char *) * bufsize);
+	char *token;
+
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Error: malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, DELIM);
+	do {
+		tokens[position] = token;
+		position++;
+
+		if (position >= bufsize)
+		{
+			busize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (tokens == NULL)
+			{
+				fprintf(stderr, "Error: realloc");
+				exit(EXIT_FAILURE);
+			}
+		}
+	} while (token = strtok(NULL, DELIM));
+	tokens[position] = NULL;
+
+	return (tokens);
+}
