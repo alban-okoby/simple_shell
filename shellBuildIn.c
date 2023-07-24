c4d021e5c885e4adbfc03194fdee5919d1515ca3
Rophpad <rophenp@gmail.com>
2023-07-24 01:35:30 -0700

    Func that handle buildin cmd

---
+++ shellBuildIn.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * shellBuildIn - Call shell buildin command
+ * @parsedtxt: The command entered by user
+ *
+ * Return: 0 or 1 depending on the command
+ */
+int shellBuildIn(char **parsedtxt)
+{
+
+}
