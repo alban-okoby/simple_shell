f32a630eaa4a1b60511bed569ecaea9739b75f54
Rophpad <rophenp@gmail.com>
2023-07-26 01:54:32 -0700

    executable renamed

--- _getline.c
+++ _getline.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-#define READLINE_BUFSIZE 1024
+
 /**
  * _getline - This function s to
  * get userinput
@@ -8,39 +8,5 @@
  */
 char *_getline(void)
 {
-	int c;
-	static char buffer[READLINE_BUFSIZE];
-	int position = 0;
-	int bufsize = 0;
-	char *line = malloc(sizeof(char) * READLINE_BUFSIZE);
-
-	if (!line)
-	{
-		fprintf(stderr, "Error: malloc");
-		exit(EXIT_FAILURE);
-	}
-
-	while (1)
-	{
-		if (position >= bufsize)
-		{
-			bufsize = read(0, buffer, READLINE_BUFSIZE);
-			if (bufsize <= 0)
-			{
-				line[position] = '\0';
-				return (line);
-			}
-			position = 0;
-		}
-		c = buffer[position];
-
-		if (c == '\n')
-		{
-			line[position] = '\0';
-			position++;
-			return (line);
-		}
-		line[position] = c;
-		position++;
-	}
+	return NULL;
 }
